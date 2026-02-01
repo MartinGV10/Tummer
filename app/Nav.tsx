c55c83e415b7bbfc821f80b4926347274d75035f
MartinGV10 <mganen10@gmail.com>
2026-02-01 18:43:50 -0500

    Changed font, installed classnames, updaed nav

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -1,17 +1,22 @@
 'use client'
 import Link from 'next/link'
 import React from 'react'
+import classnames from 'classnames'
 
 const Nav = () => {
   const links = [
+    { label: 'Sign in', href: '/login'},
     { label: 'Begin Tracking', href: '/signup'},
     // { label: '', href: ''},
   ]
   return (
-    <nav className='sticky top-0 z-50 flex items-center justify-around p-6 bg-green-500 text-white font-bold text-xl'>
+    <nav className='sticky top-0 z-50 flex items-center justify-around p-6 text-l border-b-4 border-green-500 shadow-lg font-medium'>
       <Link href='/' className='text-2xl'>Tummer</Link>
-      <ul className=''>
-        {links.map(link => <Link key={link.href} href={link.href} className='bg-green-700 hover:bg-green-800 p-3 rounded-2xl'>{link.label}</Link>)}
+      <ul className='space-x-6'>
+        {links.map(link => <Link key={link.href} href={link.href} className={classnames({
+          'bg-green-600 hover:bg-green-800 text-white': link.href === '/signup',
+          'p-3 rounded-2xl transition-all text-black ': true
+        })}>{link.label}</Link>)}
       </ul>
     </nav>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex items-center justify-center mt-10">
       Hello World
     </div>
   );
