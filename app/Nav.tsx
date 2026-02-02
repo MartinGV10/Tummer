062e2a6f1efe901ebc47b250cf75cdf446088624
MartinGV10 <mganen10@gmail.com>
2026-02-01 23:55:21 -0500

    Updated nav elements

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -10,12 +10,13 @@ const Nav = () => {
     // { label: '', href: ''},
   ]
   return (
-    <nav className='sticky top-0 z-50 flex items-center justify-around p-6 text-l border-b-4 border-green-500 shadow-lg font-medium'>
-      <Link href='/' className='text-2xl'>Tummer</Link>
+    <nav className='sticky top-0 z-50 flex items-center justify-around p-6 text-l border-b-4 border-green-500 shadow-lg font-medium bg-white'>
+      <Link href='/' className='text-2xl text-black'>Tummer</Link>
       <ul className='space-x-6'>
         {links.map(link => <Link key={link.href} href={link.href} className={classnames({
           'bg-green-600 hover:bg-green-800 text-white': link.href === '/signup',
-          'p-3 rounded-2xl transition-all text-black ': true
+          'hover:text-green-800': link.href === '/login',
+          'p-3 rounded-2xl transition-all text-black': true
         })}>{link.label}</Link>)}
       </ul>
     </nav>
