e5e96785364660bb147115e1826bf700d4003d4e
MartinGV10 <mganen10@gmail.com>
2026-02-03 16:22:37 -0500

    Added a nav bar on successful login

---
+++ app/components/UserNav.tsx
@@ -0,0 +1,73 @@
+'use client'
+import React from 'react'
+import { IconLeaf } from '@tabler/icons-react'
+import classnames from 'classnames'
+import Link from 'next/link'
+import { Avatar, DropdownMenu, Button } from '@radix-ui/themes'
+
+type Profile = {
+  first_name: string
+  last_name: string
+  username: string
+  reason: string | null
+  avatar_url: string | null
+}
+
+type UserNavProps = {
+    profile: Profile
+}
+
+const UserNav: React.FC<UserNavProps> = ({ profile }) => {
+  const links = [
+    { label: 'Track Meals', href: '/track'},
+    { label: 'Log Foods', href: '/log'},
+    { label: 'Insights', href: '/insights'},
+    { label: 'Community', href: '/community'},
+  ]
+  return (
+    <nav className="
+        sticky top-0 z-50 flex items-center justify-around p-6 text-l font-medium bg-white shadow-lg relative after:absolute after:bottom-0 after:left-0 after:h-1 after:w-full after:bg-gradient-to-r after:from-green-400 after:via-emerald-400 after:to-green-600
+      ">
+
+      <Link href='/dashboard' className='text-2xl flex gap-2 font-medium items-center'>Tummer <IconLeaf size={30}></IconLeaf></Link>
+
+      <ul className='flex items-center justify-center gap-2'>
+        {links.map(link => <Link key={link.href} href={link.href} className={classnames({
+          'bg-green-600 hover:bg-green-800 text-white': link.href === '/signup',
+          'hover:text-green-800': link.href === '/login',
+          'p-3 rounded-2xl transition-all text-black': true
+        })}>{link.label}</Link>)}
+
+        <div>
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger asChild>
+              <button className="rounded-full p-0 border-none bg-transparent cursor-pointer focus:outline-none">
+                <Avatar
+                  size="4"
+                  radius="full"
+                  src={profile.avatar_url ?? undefined}
+                  fallback={profile.first_name[0]}
+                  color="green"
+                />
+              </button>
+            </DropdownMenu.Trigger>
+
+            <DropdownMenu.Content>
+              <DropdownMenu.Item>Settings</DropdownMenu.Item>
+              <DropdownMenu.Item>Upgrade Plan</DropdownMenu.Item>
+              <DropdownMenu.Separator />
+              <DropdownMenu.Item>Help</DropdownMenu.Item>
+              <DropdownMenu.Separator />
+              <DropdownMenu.Item color="red">
+                Logout
+              </DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Root>
+        </div>
+
+      </ul>
+    </nav>
+  )
+}
+
+export default UserNav
